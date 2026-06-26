fb21f10b77644677c37390f0cec628af3094127c
Sahil Bagul <joyboy7641@gmail.com>
2026-06-26 18:43:41 +0530

    chore: hardcode clarity project ID for instant tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -237,17 +237,15 @@ export default function RootLayout({
         `}
       </Script>
       {/* Microsoft Clarity Tracking */}
-      {process.env.NEXT_PUBLIC_CLARITY_ID && (
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
-          `}
-        </Script>
-      )}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "xczzb11mqw");
+        `}
+      </Script>
       <body className={clsx(inter.variable, handwriting.variable, "bg-white text-bond-navy antialiased font-sans min-h-screen flex flex-col")}>
         {/* Permanent 3px bond-lime top accent bar */}
         <div className="fixed top-0 left-0 right-0 h-[3px] bg-bond-lime z-[9999] pointer-events-none" />
